fc2b678fd30ee0e5286127d224b5f297cb649f15
mngv7 <zackariya.taylor@gmail.com>
2024-07-30 18:33:02 +1000

    Under the Main class the onLogIn method is now complete and calls the run menu on the instance of a user.

--- src/Main.java
+++ src/Main.java
@@ -78,6 +78,9 @@ public class Main {
         User user = authService.logIn(username, password);
         System.out.println("Welcome, " + user.getUsername() + "!");
         // TODO Later: Add the to-do list operations
+        // TODO Now: Create an instance of the ToDoList class with the logged-in user and call the run method
+        ToDoList userToDoList = new ToDoList(user);
+        userToDoList.run();
     }
 
     /**
